8b788a910dcba78ba45abf3e461944fae73a8a52
Santosh <santosh.bm889@gmail.com>
2019-10-10 14:41:43 +0530

    Only Selenium Practice Code

---
+++ SeleniumPracticeCode/src/retryLogic/FailedTestcaseExecution.java
@@ -0,0 +1,13 @@
+package retryLogic;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FailedTestcaseExecution {
+	@Test(retryAnalyzer = RetryAnalyzer.class)
+	public void test01() {
+		Assert.assertEquals(false, true);
+	}
+}
